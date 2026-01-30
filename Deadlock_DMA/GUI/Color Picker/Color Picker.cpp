909cdd0da6a0346fc073edd31fc59d8249ccc044
abbey <61197745+affectioned@users.noreply.github.com>
2026-01-30 01:22:38 +0100

    Update Color Picker.cpp
    fix conflicting ids ImGui error

--- Deadlock_DMA/GUI/Color Picker/Color Picker.cpp
+++ Deadlock_DMA/GUI/Color Picker/Color Picker.cpp
@@ -9,40 +9,40 @@ void ColorPicker::Render()
 	ImGui::Begin("Color Picker", &bMasterToggle);
 
 	// HiddenKing Team Colors
-	if (ImGui::CollapsingHeader("HiddenKing Team", ImGuiTreeNodeFlags_DefaultOpen))
-	{
-		MyColorPicker("Name Tag", ColorPicker::HiddenKingNameTagColor);
-		MyColorPicker("Bone", ColorPicker::HiddenKingBoneColor);
-		MyColorPicker("Radar", ColorPicker::HiddenKingRadarColor);
-		MyColorPicker("Trooper", ColorPicker::HiddenKingTrooperColor);
-		MyColorPicker("Health Status Bar", ColorPicker::HiddenKingHealthStatusBarColor);
-		MyColorPicker("Souls Status Bar", ColorPicker::HiddenKingSoulsStatusBarColor);
-	}
+	ImGui::SeparatorText("HiddenKing Team");
+	ImGui::PushID("HiddenKing");
+	MyColorPicker("Name Tag", ColorPicker::HiddenKingNameTagColor);
+	MyColorPicker("Bone", ColorPicker::HiddenKingBoneColor);
+	MyColorPicker("Radar", ColorPicker::HiddenKingRadarColor);
+	MyColorPicker("Trooper", ColorPicker::HiddenKingTrooperColor);
+	MyColorPicker("Health Status Bar", ColorPicker::HiddenKingHealthStatusBarColor);
+	MyColorPicker("Souls Status Bar", ColorPicker::HiddenKingSoulsStatusBarColor);
+	ImGui::PopID();
+	ImGui::Spacing();
 
 	// Archmother Team Colors
-	if (ImGui::CollapsingHeader("Archmother Team", ImGuiTreeNodeFlags_DefaultOpen))
-	{
-		MyColorPicker("Name Tag", ColorPicker::ArchmotherNameTagColor);
-		MyColorPicker("Bone", ColorPicker::ArchmotherBoneColor);
-		MyColorPicker("Radar", ColorPicker::ArchmotherRadarColor);
-		MyColorPicker("Trooper", ColorPicker::ArchmotherTrooperColor);
-		MyColorPicker("Health Status Bar", ColorPicker::ArchmotherHealthStatusBarColor);
-		MyColorPicker("Souls Status Bar", ColorPicker::ArchmotherSoulsStatusBarColor);
-	}
+	ImGui::SeparatorText("Archmother Team");
+	ImGui::PushID("Archmother");
+	MyColorPicker("Name Tag", ColorPicker::ArchmotherNameTagColor);
+	MyColorPicker("Bone", ColorPicker::ArchmotherBoneColor);
+	MyColorPicker("Radar", ColorPicker::ArchmotherRadarColor);
+	MyColorPicker("Trooper", ColorPicker::ArchmotherTrooperColor);
+	MyColorPicker("Health Status Bar", ColorPicker::ArchmotherHealthStatusBarColor);
+	MyColorPicker("Souls Status Bar", ColorPicker::ArchmotherSoulsStatusBarColor);
+	ImGui::PopID();
+	ImGui::Spacing();
 
 	// Neutral/Shared Colors
-	if (ImGui::CollapsingHeader("Neutral & Shared", ImGuiTreeNodeFlags_DefaultOpen))
-	{
-		MyColorPicker("Sinner's Sacrifice", ColorPicker::SinnersColor);
-		MyColorPicker("Monster Camp", ColorPicker::MonsterCampColor);
-		MyColorPicker("Unsecured Souls Text", ColorPicker::UnsecuredSoulsTextColor);
-		MyColorPicker("Unsecured Souls Highlighted Text", ColorPicker::UnsecuredSoulsHighlightedTextColor);
-		MyColorPicker("Health Bar Foreground", ColorPicker::HealthBarForegroundColor);
-		MyColorPicker("Health Bar Background", ColorPicker::HealthBarBackgroundColor);
-		MyColorPicker("Aimbot FOV Circle", ColorPicker::AimbotFOVCircle);
-		MyColorPicker("Aimbot FOV Circle Active", ColorPicker::AimbotFOVCircleActive);
-		MyColorPicker("Radar Background", ColorPicker::RadarBackgroundColor);
-	}
+	ImGui::SeparatorText("Neutral & Shared");
+	MyColorPicker("Sinner's Sacrifice", ColorPicker::SinnersColor);
+	MyColorPicker("Monster Camp", ColorPicker::MonsterCampColor);
+	MyColorPicker("Unsecured Souls Text", ColorPicker::UnsecuredSoulsTextColor);
+	MyColorPicker("Unsecured Souls Highlighted Text", ColorPicker::UnsecuredSoulsHighlightedTextColor);
+	MyColorPicker("Health Bar Foreground", ColorPicker::HealthBarForegroundColor);
+	MyColorPicker("Health Bar Background", ColorPicker::HealthBarBackgroundColor);
+	MyColorPicker("Aimbot FOV Circle", ColorPicker::AimbotFOVCircle);
+	MyColorPicker("Aimbot FOV Circle Active", ColorPicker::AimbotFOVCircleActive);
+	MyColorPicker("Radar Background", ColorPicker::RadarBackgroundColor);
 
 	ImGui::End();
 }
