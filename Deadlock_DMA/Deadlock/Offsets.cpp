d6b9b8547ed99641d6c721052ce290e3691c777e
abbey <61197745+affectioned@users.noreply.github.com>
2026-06-05 22:34:23 +0200

    update offsets for latest game patch
    m_hController shifted 0x10A8→0x10B0, m_nCurrencies 0x12D8→0x12E0,
    m_nUnsecuredSouls 0x12EC (index 3 of shifted array). Fallback RVAs
    updated for GameEntitySystem, LocalController, ViewMatrix, CPrediction.

--- Deadlock_DMA/Deadlock/Offsets.cpp
+++ Deadlock_DMA/Deadlock/Offsets.cpp
@@ -46,19 +46,19 @@ bool Offsets::ResolveOffsets(DMA_Connection* Conn)
 	uintptr_t clientEnd  = clientBase + Deadlock::Proc().GetModuleSize(GameModules::ClientDll);
 
 	ResolveOffset(Conn, pid, clientBase, clientEnd,
-		"GameEntitySystem", Offsets::GameEntitySystem, 0x31887F8,
+		"GameEntitySystem", Offsets::GameEntitySystem, 0x38A7C98,
 		"48 8B 0D ? ? ? ? 8B FD C1 EF", 3, 7);
 
 	ResolveOffset(Conn, pid, clientBase, clientEnd,
-		"LocalController", Offsets::LocalController, 0x3708D90,
+		"LocalController", Offsets::LocalController, 0x2DC5258,
 		"48 3B 35 ? ? ? ? 75 ? 48 C7 05", 3, 7);
 
 	ResolveOffset(Conn, pid, clientBase, clientEnd,
-		"ViewMatrix", Offsets::ViewMatrix, 0x373DDB0,
+		"ViewMatrix", Offsets::ViewMatrix, 0x3789CA0,
 		"F3 0F 10 05 ? ? ? ? F3 0F 59 01", 4, 8);
 
 	ResolveOffset(Conn, pid, clientBase, clientEnd,
-		"CPrediction", Offsets::Prediction, 0x2E95F98,
+		"CPrediction", Offsets::Prediction, 0x2EAEC98,
 		"48 8B 05 ? ? ? ? 38 58", 3, 7);
 
 	DbgLog("All offsets resolved.");
